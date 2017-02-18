0884875be8fa5b91cef364c5bfb0630851792e93
Seongjun Kim <isair.zun@gmail.com>
2017-02-18 14:52:42 +0900

    Add vue-horizon

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import router from './plugins/router'
+import horizon from './plugins/horizon'
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  horizon,
   template: '<App/>',
   components: { App }
 })
---
+++ src/plugins/horizon.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Horizon from 'vue-horizon'
+
+Vue.use(Horizon, {
+  host: '127.0.0.1:8181',
+  models: ['messages']
+})
