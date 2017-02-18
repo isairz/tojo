1712f11520923baf26fbc1d88742b793d2cad91d
Seongjun Kim <isair.zun@gmail.com>
2017-02-18 22:48:23 +0900

    Add Navbar and Sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <navbar></navbar>
+    <sidebar></sidebar>
+    <section class="app-view">
+      <transition name="fade" mode="out-in">
+        <router-view class="container is-fluid"></router-view>
+      </transition>
+    </section>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
+import Sidebar from './components/Sidebar.vue'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Navbar,
+    Sidebar
+  }
 }
 </script>
 
 <style lang="sass">
 @import './style.sass'
+
+.app-view
+  padding-top: 50px
+  margin-left: 180px
+  transform: translateZ(0)
+  .container
+    margin: 0
+    padding: 20px
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="hero is-bold app-navbar">
+    <div class="hero-head">
+      <nav class="nav">
+        <div class="nav-left">
+          <a class="nav-item is-hidden-tablet">
+            <i class="fa fa-bars" aria-hidden="true"></i>
+          </a>
+        </div>
+        <div class="nav-center">
+          <router-link to="/" exact class="nav-item is-brand title is-4" href="/">
+            <strong>Tojo</strong>
+          </router-link>
+        </div>
+        <div class="nav-right is-flex"></div>
+      </nav>
+    </div>
+  </section>
+</template>
+
+<style lang="sass">
+@import '~bulma/sass/utilities/variables'
+.app-navbar
+  position: fixed
+  min-width: 100%
+  z-index: 1024
+  box-shadow: 0 2px 3px rgba(17, 17, 17, 0.1), 0 0 0 1px rgba(17, 17, 17, 0.1)
+</style>
