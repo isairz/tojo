42054256cf08c0e9270fb233b0db32ce5771fd5f
Seongjun Kim <isair.zun@gmail.com>
2017-02-19 03:18:03 +0900

    Remove handsontable

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from 'views/Hello'
-import Table from 'views/Table'
 import Task from 'views/Task'
 
 Vue.use(Router)
@@ -12,11 +11,6 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
-    },
-    {
-      path: '/table',
-      name: 'Table',
-      component: Table
     },
     {
       path: '/task',
